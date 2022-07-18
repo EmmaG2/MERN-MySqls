115913e69f1db32eb9f6af7e98b0519c22b97494
EmmanuelRdezG2 <itsmeemmadev@gmail.com>
2022-07-18 16:05:29 -0500

    router created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 import TasksPage from "./pages/tasksPage";
+import TaskForm from "./pages/taksForm";
+import NotFound from "./pages/notFound";
+import { Fragment } from "react";
+
+import Navbar from "./components/navbar";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<TasksPage />} />
-    </Routes>
+    <Fragment>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<TasksPage />} />
+        <Route path="/new" element={<TaskForm />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Fragment>
   );
 }
 
